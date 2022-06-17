1764540ac95a8ae7a44c202e6b7b116a0e208181
Tiiiagooo <andre.decarvalhobene@gmail.com>
2022-06-17 08:57:16 +0200

    code a corriger

--- Code a corriger.py
+++ Code a corriger.py
@@ -54,74 +54,22 @@ for i in chemin:
         vrai_entite_nomme.append(lieu)
 
 
-# In[42]:
+# In[ ]:
 
 
-import json
-import tqdm
-corpus = ouvrir_json("tmp.json")
-
-
-dic_longueur_chanson = {}
-
-for nom_artiste, donnee in tqdm.tqdm(corpus.items()):
-    #dic_longueur_chanson = {nom_artiste:{}}
-    dic_longueur_chanson.setdefault(nom_artiste, {})
-    for element in donnee:
-        #chansons = []
-        try:
-            #txt = str(parole)
-            parole = element["lyrics"]
-            txt = Splittxt3(parole)
-            longueur = len(txt)
-            
-            #nbre_de_texte_traitable += 1
-        except:
-            longueur = 0
-        titre = element["full_title"]
-        #print(nom_artiste, titre, len(txt))
-        dic_longueur_chanson[nom_artiste].setdefault(titre, [])
-        dic_longueur_chanson[nom_artiste][titre].append(longueur)
-
-with open("Data/Dictionnaire_longueur_chansons.json", "w") as w: 
-        w.write(json.dumps(dic_longueur_chanson, indent=2))
 
 
 
 # In[77]:
 
 
-longueur_for_graph = {}
-for nom_artiste, dic in dic_longueur_chanson.items():
-    longueur_for_graph[nom_artiste] = {}
-    for titre, longueur_titre in sorted(dic.items(), key=lambda x: x[1], reverse=False):
-        longueur_for_graph[nom_artiste][titre] = longueur_titre
 
 
-# In[85]:
+
+# In[ ]:
 
 
-import matplotlib.pyplot as pyplot
-#print(dic_longueur_chanson.keys())
-for nom_artiste, dic in dic_longueur_chanson.items():
-    if nom_artiste == "_113":
-        liste_vide = []
-        
-        #for titre, longueur_chanson in dic.items():
-            liste_vide.append(dic[1][i])
-            
-            #for chiffre in longueur_chanson:
-                #liste_vide.append(chiffre)
-        fig, ax1 = pyplot.subplots()
-        ax1.set_ylabel("Nombre de mots")
-        ax1.plot(liste_vide, 'b-')
 
-        ax1.set_xlabel('longueur des titres de %s'%nom_artiste, color='b')
-
-
-        fig.tight_layout()
-        pyplot.savefig("Data/image memoire/%s.png"%nom_artiste)
-        pyplot.show()
 
 
 # In[ ]:
@@ -134,47 +82,7 @@ for nom_artiste, dic in dic_longueur_chanson.items():
 # In[ ]:
 
 
-artiste = []
-toutes_les_chansons = []
-tous_les_mots = []
-nbre_de_texte_traitable = 0
-nbre_de_texte_non_traitable = 0
-nbre_de_mot_totale = 0
-nbre_de_mot_par_chanson = []
-nbre_mot_totale = []
-moyenne_toto = []
-for i in moyenne_toto:
-    moyenne_totale = sum(i)/len(moyenne_toto)
-    print(chanteur, moyenne_totale)
-chansons.append(parole)
-            toutes_les_chansons.append(chansons)
-            mots = Splittxt3(parole)
-            #print(mots)
-            nbre_mot_totale.append(mots)
-            moyenne_mot = sum(len(mots))/len(nbre_mot_toto)
-            moyenne_toto.append(moyenne_mot)
-for liste_chansons in tqdm.tqdm(toutes_les_chansons):
-    for chanson in liste_chansons:
-        try:
-            mots = Splittxt3(chanson)
-            tous_les_mots.append(mots)
-        except:
-            tous_les_mots.append([])
-        if chanson != None:
-            nbre_de_texte_traitable += 1
-        else:
-            nbre_de_texte_non_traitable += 1
-    #artiste.append(chanteur)
-#print(nbre_de_texte_non_traitable)
-#print(nbre_de_texte_traitable)
-#print(tous_les_mots)
-for liste_mots in tqdm.tqdm(tous_les_mots):
-    nbre_de_mot_par_chanson.append(len(liste_mots))
-    nbre_de_mot_par_chanson.append(0)
-    for mot in liste_mots:
-        nbre_de_mot_totale += 1
-#print(nbre_de_mot_par_chanson)
-#print(nbre_de_mot_totale)
+
 
 
 # In[3]:
@@ -235,7 +143,7 @@ print(to_map)
 # In[ ]:
 
 
-1/0
+
             
 
 
